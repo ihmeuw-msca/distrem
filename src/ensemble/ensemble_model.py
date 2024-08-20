a0f2567d4105314c55c9728fd8891517dba37906
mbi6245 <mbi6245@uw.edu>
2024-08-20 16:44:58 -0700

    temporarily remove jax implementation

--- src/ensemble/ensemble_model.py
+++ src/ensemble/ensemble_model.py
@@ -5,9 +5,10 @@ import numpy.typing as npt
 import scipy.linalg as linalg
 import scipy.optimize as opt
 import scipy.stats as stats
-from jaxopt import ScipyBoundedMinimize
 
 from ensemble.distributions import distribution_dict
+
+# from jaxopt import ScipyBoundedMinimize
 
 
 class EnsembleModel:
@@ -347,18 +348,18 @@ class EnsembleFitter:
         # initialize equal weights for all dists and optimize
         initial_guess = np.zeros(num_distributions) + 1 / num_distributions
         bounds = tuple((0, 1) for i in range(num_distributions))
-        # TODO: IMPLEMENT WITH JAX INSTEAD
-        minimizer_result = ScipyBoundedMinimize(
-            fun=self.ensemble_func, args=(ecdf, cdfs), method="l-bfgs-b"
-        ).run(initial_guess, bounds=bounds)
-        # minimizer_result = opt.minimize(
-        #     fun=self.ensemble_func,
-        #     x0=initial_guess,
-        #     args=(ecdf, cdfs),
-        #     bounds=bounds,
-        #     # options={"disp": True},
-        # )
-        fitted_weights = minimizer_result.params
+        # minimizer_result = ScipyBoundedMinimize(
+        #     fun=self.ensemble_func, args=(ecdf, cdfs), method="l-bfgs-b"
+        # ).run(initial_guess, bounds=bounds)
+        # fitted_weights = minimizer_result.params
+        minimizer_result = opt.minimize(
+            fun=self.ensemble_func,
+            x0=initial_guess,
+            args=(ecdf, cdfs),
+            bounds=bounds,
+            # options={"disp": True},
+        )
+        fitted_weights = minimizer_result.x
 
         res = EnsembleResult(
             # weights=tuple(
